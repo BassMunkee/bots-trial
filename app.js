1fb85cb55a743978d0bea3be11d1fa89083089f8
Srikanth A V <srikanthav@live.com>
2017-06-05 00:40:29 +0530

    Add web ui and test end point

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+"use strict;"
+
 var restify = require('restify');
 var builder = require('botbuilder');
-var credentials = require('./msft-bot-credentials')
+var credentials = require('./msft-bot-credentials');
 
 // Setup Restify server
 var server = restify.createServer();
@@ -18,6 +20,19 @@ var connector = new builder.ChatConnector({
 
 // Listen for messages from users
 server.post('/api/messages', connector.listen());
+
+// Test server
+server.get('/test', function(req, res) {
+	res.send('Hey there!');
+});
+
+// Web portal
+server.get('/web', function(req, res) {
+	res.contentType = 'text/html';
+	res.header('Content-Type','text/html');
+	res.end("<html><body height=600><iframe src='https://webchat.botframework.com/embed/SnapChef?s=iC4DQVOePQ8.cwA.l6c._ffaBVptGaJwXp-Jl9FPemr8z3xAp9k4Sz3_iifkemI'></iframe></body></html>");
+	// res.send(__dirname + '/webchat.html');
+});
 
 // Receive messages from the user and respond by echoing each message back
 var bot = new builder.UniversalBot(connector, function(session) {
--- notes.md
+++ notes.md
@@ -7,3 +7,5 @@ curl -k -X POST https://login.microsoftonline.com/botframework.com/oauth2/v2.0/t
 curl -k -X POST https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token -d "grant_type=client_credentials&client_id=4057c402-f61e-41bc-a34a-a309cca4c604&client_secret=r3Phz4rKshp32jz9GneMSaJ&scope=https%3A%2F%2Fapi.botframework.com%2F.default"
 
 Note: Don't get confused by the naming - secret, password. There are a couple of ways to authenticate the app on the BotFramework console. In this case we are using the `password` and this has to be used as the value for the `secret` parameter.
+
+Web iFrame code - <iframe src='https://webchat.botframework.com/embed/SnapChef?s=iC4DQVOePQ8.cwA.l6c._ffaBVptGaJwXp-Jl9FPemr8z3xAp9k4Sz3_iifkemI'></iframe>
